8e04e56cd1a32735e24394a7a9e01302f7d733f8
wendy munyasi <wendymunyasi@gmail.com>
2022-05-31 21:45:28 +0300

    Fix incorrect output for neg numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_digit = (number * -1) % 10
+    new_number = number * -1
+    last_digit =  (-1) * (new_number % 10)
 else:
     last_digit = number % 10
 if last_digit > 5:
