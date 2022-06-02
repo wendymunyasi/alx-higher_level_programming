0c5eb3452aec85303b190429c5a0397067f2d85c
wendy munyasi <wendymunyasi@gmail.com>
2022-06-02 22:02:15 +0300

    Fix string formatting

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     b = 5
 
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
