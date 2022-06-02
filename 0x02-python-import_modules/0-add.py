db1856ca23aa907c8bc718df6ba6af884bc9d429
wendy munyasi <wendymunyasi@gmail.com>
2022-06-02 21:51:13 +0300

    Fix task0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
 
-a = 1
-b = 2
+    from add_0 import add
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a,b)))
