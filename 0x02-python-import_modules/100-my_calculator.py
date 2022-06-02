15b9e9a22874c6eacfd0694712c5630e1395be13
wendy munyasi <wendymunyasi@gmail.com>
2022-06-02 23:39:12 +0300

    Add a program that imports all functions in calculator_1

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from calculator_1 import add, mul, div, sub
+    from sys import argv, exit
+
+    addition = 0
+
+    # excludes name of file
+    number_of_args = len(argv) - 1
+
+    if number_of_args != 3:
+        print("Usage: ./100-my_calculator.py <a> <operat> <b>")
+        exit(1)
+
+    operat = argv[2]
+
+    if operat != '+' and operat != '-' and operat != '*' and operat != '/':
+        print("Unknown operat. Available operats: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if operat == '+':
+        print("{:d} {} {:d} = {}".format(a, argv[2], b, add(a, b)))
+    elif operat == '-':
+        print("{:d} {} {:d} = {}".format(a, argv[2], b, sub(a, b)))
+    elif operat == '*':
+        print("{:d} {} {:d} = {}".format(a, argv[2], b, mul(a, b)))
+    else:
+        print("{:d} {} {:d} = {}".format(a, argv[2], b, div(a, b)))
