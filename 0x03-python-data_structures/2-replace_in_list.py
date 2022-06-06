8067963254b181870d029c67f52ae9fd6c5840a5
wendy munyasi <wendymunyasi@gmail.com>
2022-06-06 20:54:17 +0300

    task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from syslog import LOG_EMERG
+
+
+def replace_in_list(my_list, idx, element):
+    """function that replaces an element at specific position
+    
+    If idx is negative, the function should not modify anything, and
+    returns the original list
+    If idx is out of range (> of number of element in my_list), the
+    function should not modify anything, and returns the original list
+    You are not allowed to import any module
+    You are not allowed to use try/except
+    """
+    if my_list:
+        if idx < 0:
+            return my_list
+        elif idx > len(my_list) - 1:
+            return my_list
+        else:
+            my_list[idx] = element
+        return my_list
