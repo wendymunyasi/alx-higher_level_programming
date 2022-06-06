193c48ce615859c37254a89f139fb61cb85de06a
wendy munyasi <wendymunyasi@gmail.com>
2022-06-06 22:30:54 +0300

    Fix code fail task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -9,8 +9,7 @@ def no_c(my_string):
     """
     list_1 = ['c', 'C']
 
-    if my_string:
-        new_str = my_string[:]
-        new_str = "".join(i for i in my_string if i not in list_1)
+    new_str = my_string[:]
+    new_str = "".join(i for i in my_string if i not in list_1)
 
     return new_str
