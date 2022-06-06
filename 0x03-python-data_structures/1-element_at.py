cdc8f36a44b7f6e17379d9bfe0513e3a1e75afcd
wendy munyasi <wendymunyasi@gmail.com>
2022-06-06 20:30:12 +0300

    Add a func that retrieves elem from list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    """retrieve element function
+
+    If idx is negative, the function should return None
+    If idx is out of range (> of number of element in my_list), return None
+    You are not allowed to import any module
+    You are not allowed to use try/except
+    """
+    if my_list:
+        if idx < 0:
+            return
+        elif idx > len(my_list):
+            return
+        else:
+            return my_list[idx]
