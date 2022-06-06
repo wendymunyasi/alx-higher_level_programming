41da8c552d58bc35e3c94a946c27f365578ca7c6
wendy munyasi <wendymunyasi@gmail.com>
2022-06-06 21:28:48 +0300

    Add a func that replaces an elem in a lst

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """function that replaces an element in list w/o modifying the list
+
+    If idx is negative, return a copy of the original list
+    If idx is out of range (> of number of element in my_list),
+    return a copy of the original list
+    You are not allowed to import any module
+    You are not allowed to use try/except
+    """
+
+    if my_list:
+        if idx < 0:
+            return my_list
+        elif idx > len(my_list) -1:
+            return my_list
+        else:
+            new_list = my_list
+            new_list[idx] = element
+            return new_list
