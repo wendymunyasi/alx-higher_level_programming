d87db07fc0b5a474a216280d7a6a57c10758e2a5
wendy munyasi <wendymunyasi@gmail.com>
2022-06-06 21:21:35 +0300

    Attempt to fix code fail task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -9,9 +9,12 @@ def print_reversed_list_integer(my_list=[]):
     You are not allowed to cast integers into strings
     You have to use str.format() to print integers
     """
-    # point i to the last element
-    i = len(my_list) - 1
 
-    while i >= 0:
-        print("{:d}".format(my_list[i]))
-        i -= 1
+    if my_list:
+
+        # point i to the last element
+        i = len(my_list) - 1
+
+        while i >= 0:
+            print("{:d}".format(my_list[i]))
+            i -= 1
