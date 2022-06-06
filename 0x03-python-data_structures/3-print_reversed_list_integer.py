8ebeb1a57555d517d9e1deaf39e42f063b63684e
wendy munyasi <wendymunyasi@gmail.com>
2022-06-06 21:17:13 +0300

    Add a func that prints a list in reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+def print_reversed_list_integer(my_list=[]):
+    """function that prints list in reverse
+
+    One integer per line
+    You are not allowed to import any module
+    You can assume that the list only contains integers
+    You are not allowed to cast integers into strings
+    You have to use str.format() to print integers
+    """
+    # point i to the last element
+    i = len(my_list) - 1
+
+    while i >= 0:
+        print("{:d}".format(my_list[i]))
+        i -= 1
