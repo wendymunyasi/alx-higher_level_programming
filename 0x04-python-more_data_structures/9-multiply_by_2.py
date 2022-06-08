f2bc507b6395013bcffb8a53ecf5a30085fda3cd
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 01:40:38 +0300

    Add a function that returns a new dict

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def multiply_by_2(a_dictionary):
+    """returns a new dictionary with all values multiplied by 2
+    You can assume that all values are only integers
+    Returns a new dictionary
+    You are not allowed to import any module
+    """
+    new_dict = a_dictionary.copy()
+    dict_keys = list(new_dict.keys())
+
+    for i in dict_keys:
+        i *= 2
+
+    return new_dict
