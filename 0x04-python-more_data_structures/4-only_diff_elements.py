aa42f4e5c9afe15598ace32f58694ffb881ed25e
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 00:07:12 +0300

    Add a func that returns symmetric difference of 2 sets

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    """returns a set of all elements present in only one set
+    You are not allowed to import any module
+    """
+    return set_1.symmetric_difference(set_2)
