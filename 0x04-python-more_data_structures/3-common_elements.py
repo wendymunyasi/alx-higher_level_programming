a892dd99d1adf22c2965e00dd5b37b6659aa23d7
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 00:03:32 +0300

    Add a func that returns a set of common elements in two sets

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """returns a set of common elements in two sets
+    You are not allowed to import any module
+    """
+    return set_1.intersection(set_2)
