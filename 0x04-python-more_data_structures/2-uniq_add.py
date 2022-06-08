e29c459ca0d7775bc74ddf26acb4f27937dd171d
wendy munyasi <wendymunyasi@gmail.com>
2022-06-08 23:47:20 +0300

    Add a func that adds all uniq integers in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from unittest import result
+
+
+def uniq_add(my_list=[]):
+    """adds all unique integers in a list (only once for each integer)
+    You are not allowed to import any module
+    """
+    new_list = list(set(my_list))
+    result = 0
+
+    # -1 picks the last element in the returned list below
+    return [result := result + num for num in new_list][-1]
