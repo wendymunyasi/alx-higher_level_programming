2e6cea9ca4330cf01a9d5103aec355ff0d1e664d
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 00:53:54 +0300

    Add a func that prints a dict ordered by keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -10,8 +10,8 @@ def print_sorted_dictionary(a_dictionary):
     You are not allowed to import any module
     """
 
-    # for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]):
-    #     print("{} : {}".format(key, value))  ## alternative
+    for key, value in sorted(a_dictionary.items(), key=lambda x: x[0]):
+        print("{} : {}".format(key, value))  # alternative
 
-    for key, value in sorted(a_dictionary.items()):
-        print("{} : {}".format(key, value))
+    # for key, value in sorted(a_dictionary.items()):
+    #     print("{} : {}".format(key, value))
