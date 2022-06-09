672e5cc3c5cdc5401a783506756c08de4ad45c6a
wendy munyasi <wendymunyasi@gmail.com>
2022-06-09 12:49:02 +0300

    Fix task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -8,10 +8,12 @@ def weight_average(my_list=[]):
     if not my_list:
         return 0
 
-    tuple_sum = 0
-    for sub in my_list:
-        tuple_sum += sum(sub)
+    weight_sum = 0
+    score_product = 0
 
-    result = tuple_sum / len(my_list)
+    for sub in my_list:
+        score_product += sub[0] * sub[1]
+        weight_sum += sub[1]
+    result = score_product / weight_sum
 
     return result
