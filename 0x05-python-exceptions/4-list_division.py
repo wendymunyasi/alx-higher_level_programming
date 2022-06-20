4640b8d5199de291294fe0367c34bf58270dbba1
wendy munyasi <wendymunyasi@gmail.com>
2022-06-20 20:04:05 +0300

    Add  a func that divides elem by elem in 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """function that divides element by element 2 lists.
+
+    Args:
+        my_list_1: list 1
+        my_list_2: list 2
+        list_length: length of list
+    
+    Returns: New list with all the divisions
+    """
+    new_list = []
+
+    for item in range(list_length):
+        try:
+            quotient = my_list_1[item] / my_list_2[item]
+        except IndexError:
+            print("out of range")
+        except TypeError:
+            quotient = 0
+        except ZeroDivisionError:
+            print("division by 0")
+        finally:
+            new_list.append(quotient)
+    return new_list
