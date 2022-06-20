002a95310ddd985699f1fc5cedcf6911e100d8af
wendy munyasi <wendymunyasi@gmail.com>
2022-06-20 20:34:33 +0300

    Add a function that raises a name exception with message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def raise_exception_msg(message=""):
+    """function that raises a name exception with a message.
+
+    You are not allowed to import any module
+
+    Args:
+        message: string of message to be raised
+    """
+    raise NameError(message)
