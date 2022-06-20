e6ac7b0a9cd45f9eec69b516b949424655d3f875
wendy munyasi <wendymunyasi@gmail.com>
2022-06-20 21:28:28 +0300

    Fix pycodestyle errors

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,13 +9,13 @@ def magic_calculation(a, b):
     """
     result = 0
 
-    for num in range(1,3):
+    for num in range(1, 3):
         try:
             if num > a:
                 raise Exception("Too far")
             else:
                 result += (a ** b) / num
-        except:
+        except Exception:
             result = b + a
             break
     return result
