d058b5c44bfe75ae30695898a9d9b8a1c7a44293
wendy munyasi <wendymunyasi@gmail.com>
2022-06-20 20:14:55 +0300

    Fix codefails in task 4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -19,6 +19,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             quotient = 0
         except TypeError:
+            print("wrong type")
             quotient = 0
         except ZeroDivisionError:
             print("division by 0")
