b5178a8dd9bbbb40b2ba59fa1a763ff90d593ed0
wendy munyasi <wendymunyasi@gmail.com>
2022-06-20 21:26:42 +0300

    Add some weird magic_Calculation code

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    """function that does exactly the same as the given Python bytecode.
+
+    Args:
+        a: integer
+        b: integer
+    """
+    result = 0
+
+    for num in range(1,3):
+        try:
+            if num > a:
+                raise Exception("Too far")
+            else:
+                result += (a ** b) / num
+        except:
+            result = b + a
+            break
+    return result
