8a1779cd19aa8a21995ae3f98fdf69b580330b68
wendy munyasi <wendymunyasi@gmail.com>
2022-06-21 14:34:44 +0300

    Add a class square for task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Class that defines a square by: (based on 0-square.py)
+    """
+    def __init__(self, size):
+        """Defines properties for the square
+
+        Args:
+            size: size of the square.
+        """
+        self.__private_memeber = size
