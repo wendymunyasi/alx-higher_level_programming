674744afef21a461b200407cfc6027823835efed
wendy munyasi <wendymunyasi@gmail.com>
2022-06-21 14:56:30 +0300

    Fix codefail task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,6 +17,6 @@ class Square:
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
