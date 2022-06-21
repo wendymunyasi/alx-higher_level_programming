a2bf8355bf3f4847762d87ac0db360d9fe0c2fc6
wendy munyasi <wendymunyasi@gmail.com>
2022-06-21 17:37:17 +0300

    Fix codefails task 6 - Attempt 5

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -81,9 +81,10 @@ class Square:
 
         if self.__size == 0:
             print()
-        for j in range(self.__position[1]):
-            print()
-        for i in range(self.__size):
-            for k in range(self.__position[0]):
-                print(" ",  end="")
-            print("#" * (self.__size))
+        else:
+            for j in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(" ",  end="")
+                print("#" * (self.__size))
