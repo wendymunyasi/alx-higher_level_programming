ff360092d1233b482d8dc28c73d489c0cf9f445f
wendy munyasi <wendymunyasi@gmail.com>
2022-06-21 17:20:44 +0300

    Fix codefails task 6 - Attempt 3

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,10 +45,9 @@ class Square:
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
