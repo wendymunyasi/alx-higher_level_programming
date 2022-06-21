78cf46c38dd8cb52019fc658edb953c9c52f646d
wendy munyasi <wendymunyasi@gmail.com>
2022-06-21 16:02:08 +0300

    Add a class square for task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """
-    Class that defines properties of square by: (based on 3-square.py).
+    Class that defines properties of square by: (based on 4-square.py).
 
     Attributes:
         size: size of a square (1 side).
@@ -47,3 +47,12 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    def my_print(self):
+        """prints in stdout the square with the character #
+        """
+
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            print("#" * (self.__size))
