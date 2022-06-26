060c4c10db21022c95b7f1375418329e5ec5e88d
wendy munyasi <wendymunyasi@gmail.com>
2022-06-26 15:17:49 +0300

    Add more tests

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-add_integer = __import__('dd_integer').add_integer
+add_integer = __import__('0-add_integer').add_integer
 
+print(add_integer(float("-nan"), 4))
 print(add_integer(2, 3))
-print(add_integer(12.3, 9.8))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
