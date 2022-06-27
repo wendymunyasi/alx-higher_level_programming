e1fd3c44df668f1199cbc70813139b18e3b8f7ce
wendy munyasi <wendymunyasi@gmail.com>
2022-06-27 18:17:12 +0300

    Fix codefails task 8 - try 1

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -154,9 +154,7 @@ class Rectangle:
         area_1 = rect_1.area()
         area_2 = rect_2.area()
 
-        if area_1 > area_2:
+        if area_1 >= area_2:
             return rect_1
-        elif area_2 > area_1:
-            return rect_2
-        else:
-            return rect_1
+
+        return rect_2
