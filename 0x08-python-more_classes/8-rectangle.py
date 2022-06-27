89d9aa1612f5acd4a8c2d44593fe620918a67fb0
wendy munyasi <wendymunyasi@gmail.com>
2022-06-27 18:24:01 +0300

    Fix codefails task 8 - try 4

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -151,10 +151,10 @@ class Rectangle:
         if not isinstance(rect_2, Rectangle):
             TypeError("rect_2 must be an instance of Rectangle")
 
-        area_1 = rect_1.perimeter()
-        area_2 = rect_2.perimeter()
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
 
         if area_1 >= area_2:
             return rect_1
-        else:
+        if area_1 < area_2:
             return rect_2
