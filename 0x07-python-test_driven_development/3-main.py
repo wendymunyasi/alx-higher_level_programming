9f7035447c1beea1ab0d0b65b254fcecd35c5ee2
wendy munyasi <wendymunyasi@gmail.com>
2022-06-28 23:52:33 +0300

    Add tests for task 2

--- 0x07-python-test_driven_development/3-main.py
+++ 0x07-python-test_driven_development/3-main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 say_my_name = __import__('3-say_my_name').say_my_name
 
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
+say_my_name(None, "Breezy")
+say_my_name("Chris", "Brown")
 say_my_name("Bob")
 try:
     say_my_name(12, "White")
 except Exception as e:
     print(e)
+
