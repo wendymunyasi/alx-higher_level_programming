7bfab8afc93c67fd5cf1aebe4068a051537391e8
wendy munyasi <wendymunyasi@gmail.com>
2022-06-29 16:40:41 +0300

    Attempt to fix task 7 - try 3

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -67,13 +67,19 @@ def matrix_mul(m_a, m_b):
     len_m_a = len(m_a[0])
     len_m_b = len(m_b[0])
 
+    length = 0
+
     for element in m_a:
-        if len_m_a != len(element):
+        if length != 0 and length != len(element):
             raise TypeError(size_err.format('m_a'))
+        length = len(element)
+
+    length = 0
 
     for element in m_b:
-        if len_m_b != len(element):
+        if length != 0 and length != len(element):
             raise TypeError(size_err.format('m_b'))
+        length = len(element)
 
     if len_m_a != len(m_b):
         raise ValueError(value_err.format('m_a', 'm_b'))
