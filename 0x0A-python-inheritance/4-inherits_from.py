4ce1641ed4f5af13d43df577948a5e874ec49e9b
wendy munyasi <wendymunyasi@gmail.com>
2022-07-04 17:22:57 +0300

    Add a function that returns true - task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Defines a function inherits_from()
+"""
+
+
+def inherits_from(obj, a_class):
+    """Returns True if the object is an instance of a class that inherited,
+    (directly or indirectly) from the specified class ; otherwise False.
+
+    Args:
+        obj (a_class): object to check type.
+        a_class (type): type of type to check.
+
+    Returns:
+        boolean: True or False..
+    """
+    if type(obj) is a_class:
+        return False
+    return issubclass(type(obj), a_class)
