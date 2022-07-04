4072465e82af12d39bbe93c2ea433e02be35ddc5
wendy munyasi <wendymunyasi@gmail.com>
2022-07-04 13:31:43 +0300

    Attempt to fix some points - try 1

--- 0x07-python-test_driven_development/100-main.py
+++ 0x07-python-test_driven_development/100-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 matrix_mul = __import__('100-matrix_mul').matrix_mul
 
-m_a = [[1, 2], [3, 4, 5]]
-m_b = [[1, 2], [3, 4]]
+m_a = [[1, 2], [3, 4]]
+m_b = [[5, 6], [7, 8]]
 
 print(matrix_mul(m_a, m_b))
 print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
