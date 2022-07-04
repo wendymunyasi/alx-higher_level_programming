da5ccd892ae24883459a7dce42f0bb6acb59a1b4
wendy munyasi <wendymunyasi@gmail.com>
2022-07-04 17:32:11 +0300

    Define a class BaseGeometry - task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry.
+    """
+    def area(self):
+        """Area function.
+
+        Raises:
+            Exception: if area is not implemented.
+        """
+        raise Exception("area() is not implemented")
