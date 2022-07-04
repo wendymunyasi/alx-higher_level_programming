9aa3d2ea2b4255825e2d97bf2bdc7714596c1897
wendy munyasi <wendymunyasi@gmail.com>
2022-07-04 14:46:36 +0300

    Add documentation to function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,6 +3,15 @@
 
 
 def lookup(obj):
+    """Function that returns the list of available attributes and methods,
+    f an object
+
+    Args:
+        obj (class): object
+
+    Returns:
+        list: list of available attributes and methods of an object
+    """
     method_list = [func for func in dir(obj) if callable(getattr(obj, func))]
     return method_list
     # return dir(obj)
