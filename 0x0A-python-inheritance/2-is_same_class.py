9579e22c576ec8eee816e0ea6c668a154a05c3ac
wendy munyasi <wendymunyasi@gmail.com>
2022-07-04 16:53:43 +0300

    Add a function that returns a boolean depending on type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Defines a function that returns True if the object is exactly,
+an instance of the specified class ; otherwise False."""
+
+
+def is_same_class(obj, a_class):
+    """Returns True if the object is exactly an instance of the,
+    specified class ; otherwise False
+
+    Args:
+        obj (a_class): object to check type.
+        a_class (type): type of type to check.
+
+    Returns:
+        boolean: True or False
+    """
+    return type(obj) == a_class
