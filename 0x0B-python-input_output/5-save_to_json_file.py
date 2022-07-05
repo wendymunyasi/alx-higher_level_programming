decbc869efb5222b24e87f3576e6eec967fbd0df
wendy munyasi <wendymunyasi@gmail.com>
2022-07-05 15:34:56 +0300

    Add documentation to function

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -4,10 +4,24 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Writes an Object to a text file using JSON representation.
 
+    Args:
+        my_obj (type): Object to write to text file.
+        filename (str): name of the file.
+
+    Returns:
+        type: JSON representation.
+    """
     # serializing json
     json_object = json.dumps(my_obj)
 
+    # print("type of json_object --> {}".format(type(json_object)))
+    # print("type of my_obj --> {}".format(type(my_obj)))
+    # print("type file name --> {}".format(type(filename)))
     # writing to file
-    with open(filename, 'w') as f:
+    with open(filename, 'w', encoding="utf-8") as f:
+
+        # print("type f.write(json_object) --> {}".format(type(f.write(json_object))))
+        # or json.dump(my_obj, f)
         return f.write(json_object)
