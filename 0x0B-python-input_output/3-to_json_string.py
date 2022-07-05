e8af69781f63ec52d8bb1e0eab541b67864489eb
wendy munyasi <wendymunyasi@gmail.com>
2022-07-05 14:40:02 +0300

    Add a func that returns the JSON repr of obj (str)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module containing the function to_json_string"""
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object(string).
+
+    Args:
+        my_obj (type): object to examine.
+
+    Returns:
+        str: JSON representation of object.
+    """
+    # print("type json.dumps(my_obj)--> {}".format(type(json.dumps(my_obj))))
+    # print("type my_obj--> {}".format(type(my_obj)))
+    return json.dumps(my_obj)
