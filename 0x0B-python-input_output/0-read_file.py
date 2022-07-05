14e2a5385f89323f1b25202ef2ebad2375ca301c
wendy munyasi <wendymunyasi@gmail.com>
2022-07-05 13:49:10 +0300

    Add documentation to function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""Module that reads a file and prints to stdout"""
+"""Module containing the function read_file"""
 
 
 def read_file(filename=""):
+    """Reads a file and prints to stdout.
+
+    Args:
+        filename (str, optional): name of file to read. Defaults to "".
+    """
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data)
