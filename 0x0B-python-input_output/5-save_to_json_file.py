43108a68e833cd864be6c8ab97f4116ed0445f5e
wendy munyasi <wendymunyasi@gmail.com>
2022-07-05 15:43:52 +0300

    Fix codefail - try 4

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -13,15 +13,14 @@ def save_to_json_file(my_obj, filename):
     Returns:
         type: JSON representation.
     """
-    # serializing json
-    json_object = json.dumps(my_obj)
 
     # print("type of json_object --> {}".format(type(json_object)))
     # print("type of my_obj --> {}".format(type(my_obj)))
     # print("type file name --> {}".format(type(filename)))
     # writing to file
     with open(filename, 'w', encoding="utf-8") as f:
-
+        # serializing json
+        json_object = json.dumps(my_obj)
         # or json.dump(my_obj, f)
         f.write(json_object)
         f.close()
