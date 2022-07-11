cb27b13871df852d48f38ad75f6a3defd28c8392
wendy munyasi <wendymunyasi@gmail.com>
2022-07-11 14:09:12 +0300

    Update class Rectangle - task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -162,9 +162,20 @@ class Rectangle(Base):
                 print("#", end="")
             print()
 
-    def update(self, *args):
-        """Assigns an argument to each attribute"""
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute
+
+        Args:
+            *args (tuple): arguments.
+            **kwargs (dict): double pointer to a dictionary.
+        """
+
+        # print("args {}".format(type(args)))
+        # print("kwargs {}".format(type(kwargs)))
         if args is not None and len(args) is not 0:
             list_atrr = ['id', 'width', 'height', 'x', 'y']
             for i in range(len(args)):
                 setattr(self, list_atrr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
