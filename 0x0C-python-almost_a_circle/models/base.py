d4a0a04ee7a4b616ce4f6027e5d7672b562b1e77
wendy munyasi <wendymunyasi@gmail.com>
2022-07-11 15:23:38 +0300

    Update class Base - add static method - task 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a class Base"""
+import json
 
 
 class Base:
@@ -21,3 +22,17 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): list of dictionaries.
+
+        Returns:
+            str: jason string representation.
+        """
+        if list_dictionaries is None or list_dictionaries == "[]":
+            return "[]"
+        return json.dumps(list_dictionaries)
