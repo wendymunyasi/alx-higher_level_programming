86385eef9fdbebce38896b1544dffb660f1416e2
wendy munyasi <wendymunyasi@gmail.com>
2022-07-11 15:35:43 +0300

    Update Base class - task 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -58,3 +58,16 @@ class Base:
 
         with open(filename, 'w') as f:
             f.write(lists)
+
+    def from_json_string(json_string):
+        """ Returns the list of the JSON string representation json_string.
+
+        Args:
+            json_string (str): _description_
+
+        Returns:
+            list: JSON string representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return(json.loads(json_string))
