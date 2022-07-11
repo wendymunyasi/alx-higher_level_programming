40d5ec35de44d8e0d93a6fbaf5b5e6e7691412b1
wendy munyasi <wendymunyasi@gmail.com>
2022-07-12 01:14:11 +0300

    Add various tests in tests_square.py

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -18,6 +18,10 @@ class TestSquareMethods(unittest.TestCase):
         """ Method invoked for each test """
         Base._Base__nb_objects = 0
 
+    def tearDown(self):
+        """ Cleans up after each test """
+        pass
+
     def test_new_square(self):
         """ Test new square """
         s1 = Square(3)
@@ -28,6 +32,21 @@ class TestSquareMethods(unittest.TestCase):
         self.assertEqual(s1.y, 0)
         self.assertEqual(s1.id, 1)
 
+    def test_constructor_no_args(self):
+        """ Tests constructor with no args """
+        with self.assertRaises(TypeError) as e:
+            r = Square()
+        s = "__init__() missing 1 required positional argument: 'size'"
+        self.assertEqual(str(e.exception), s)
+
+    def test_C_constructor_many_args(self):
+        """ Tests constructor with many arguments """
+        with self.assertRaises(TypeError) as e:
+            r = Square(1, 2, 3, 4, 5)
+        s = "__init__() takes from 2 to 5 positional arguments but 6 \
+were given"
+        self.assertEqual(str(e.exception), s)
+
     def test_is_Rectangle_instance(self):
         """ Test Square is a Rectangle instance """
         s1 = Square(1)
@@ -37,6 +56,14 @@ class TestSquareMethods(unittest.TestCase):
         """ Test area method """
         s1 = Square(4)
         self.assertEqual(s1.area(), 16)
+
+    def test_area_no_args(self):
+        """ Test area method with no arguments"""
+        r = Square(5)
+        with self.assertRaises(TypeError) as e:
+            Square.area()
+        s = "area() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), s)
 
     def test_load_from_file(self):
         """ Test load JSON file """
@@ -51,6 +78,14 @@ class TestSquareMethods(unittest.TestCase):
             s1.display()
             self.assertEqual(str_out.getvalue(), result)
 
+    def test_display_no_args(self):
+        """ Test display method with no arguments """
+        r = Square(9)
+        with self.assertRaises(TypeError) as e:
+            Square.display()
+        s = "display() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), s)
+
     def test_str(self):
         """ Test __str__ return value """
         s1 = Square(3, 1, 3)
@@ -58,3 +93,11 @@ class TestSquareMethods(unittest.TestCase):
         with patch('sys.stdout', new=StringIO()) as str_out:
             print(s1)
             self.assertEqual(str_out.getvalue(), result)
+
+    def test_str_no_args(self):
+        """ Tests __str__ method with no arguments """
+        r = Square(5, 2)
+        with self.assertRaises(TypeError) as e:
+            Square.__str__()
+        s = "__str__() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), s)
