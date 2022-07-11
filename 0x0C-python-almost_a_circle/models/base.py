bf1d4537eae0fbcfd05a3be7212345b112ae1154
wendy munyasi <wendymunyasi@gmail.com>
2022-07-11 12:09:32 +0300

    Add a class Base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Base"""
+
+
+class Base:
+    """Class that defines properties of Base.
+    
+     Attributes:
+        id (int): Identity of each instance.
+    """
+    __nb_objects = 0
+    
+    def __init__(self, id=None):
+        """Creates new instances of Base.
+
+        Args:
+            id (int, optional): Identity of each instance. Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
