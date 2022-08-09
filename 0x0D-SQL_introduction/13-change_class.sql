c195d381a3328242d3710f30f84750ae0bc658f0
wendy munyasi <wendymunyasi@gmail.com>
2022-08-09 20:15:39 +0300

    Add script that removes records with score <= 5

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- script 13
+-- script that removes all records with a score <= 5 in the table
+-- second_table of the database hbtn_0c_0 in your MySQL server.
+DELETE FROM second_table WHERE score <= 5;
