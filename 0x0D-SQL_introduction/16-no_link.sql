b823b10efcaddc43a8fdc74d6e457d392a8aaadb
wendy munyasi <wendymunyasi@gmail.com>
2022-08-09 20:17:25 +0300

    Add script that lists records of tb with given conditions

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- script 16
+-- script that lists all records of the table second_table of the
+-- database hbtn_0c_0 in your MySQL server.
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
