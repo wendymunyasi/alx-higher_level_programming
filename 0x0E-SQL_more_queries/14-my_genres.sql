0d80fc0a3e5a00882ca5cd97ee17827ad138bc90
wendy munyasi <wendymunyasi@gmail.com>
2022-08-10 17:53:50 +0300

    Add script that lists all genres in db - task 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+--
+SELECT
+    tv_genres.name AS name
+    FROM tv_show_genres
+    INNER JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    INNER JOIN tv_shows
+    ON tv_shows.id = tv_show_genres.show_id
+    AND tv_shows.title = 'Dexter'
+    ORDER BY name;
