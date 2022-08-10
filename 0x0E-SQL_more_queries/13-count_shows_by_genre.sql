45a8fd6ebd3633ce7f0162f17e5a06007035a4d9
wendy munyasi <wendymunyasi@gmail.com>
2022-08-10 17:53:25 +0300

    Add script that lists all genres in db - task 13

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+--
+SELECT
+    tv_genres.name AS genre,
+    COUNT(tv_show_genres.genre_id) AS number_of_shows
+    FROM tv_show_genres
+    INNER JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    GROUP BY genre
+    ORDER BY number_of_shows DESC;
