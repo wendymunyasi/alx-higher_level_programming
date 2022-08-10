2d2faf24f918d9cef34cb611084fb1521b927886
wendy munyasi <wendymunyasi@gmail.com>
2022-08-10 15:05:01 +0300

    Add script that creates user & grants priviledges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- script 1
+-- creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
