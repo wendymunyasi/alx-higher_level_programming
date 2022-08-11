f9d1feefd1bcf27b6c249f6424cfc6cb3afb8fc9
wendy munyasi <wendymunyasi@gmail.com>
2022-08-11 10:02:48 +0300

    Corerct syntax

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,5 @@
 -- script that lists all shows contained in hbtn_0d_tvshows without
---a genre linked.
+-- a genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
     FROM tv_shows LEFT JOIN tv_show_genres
     ON tv_shows.id = tv_show_genres.show_id
