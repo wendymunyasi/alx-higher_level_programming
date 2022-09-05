b0a131fb7768d5cbc578057c0c54121b49472218
wendy munyasi <wendymunyasi@gmail.com>
2022-09-06 00:59:56 +0300

    Replace arrow func with normal - task 10 fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-let factorial = (number) => {
+function factorial (number) {
   if ((isNaN(number)) || (number === 1)) {
     return 1;
   }
   return factorial(number - 1) * number;
-};
+}
 console.log(factorial(parseInt(process.argv[2])));
