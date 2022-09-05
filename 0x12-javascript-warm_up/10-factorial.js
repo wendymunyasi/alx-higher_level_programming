6cedfd15480bd87aefbb8cb1f6065d940aa241f7
wendy munyasi <wendymunyasi@gmail.com>
2022-09-06 00:50:59 +0300

    Add func that computes factorial - task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const factorial = (number) => {
+  if ((isNaN(number)) || (number === 1)) {
+    return 1;
+  }
+  return factorial(number - 1) * number;
+};
+console.log(factorial(parseInt(process.argv[2])));
