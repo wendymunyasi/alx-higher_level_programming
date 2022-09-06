520939f5ad9944f908bb9e093a587f7cd57700df
wendy munyasi <wendymunyasi@gmail.com>
2022-09-06 21:34:49 +0300

    Add class Square inheriting frm Rectangle - task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const square = require('./5-square');
+class Square extends square {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let s = '';
+      for (let j = 0; j < this.width; j++) {
+        s += c;
+      }
+      console.log(s);
+    }
+  }
+}
+module.exports = Square;
