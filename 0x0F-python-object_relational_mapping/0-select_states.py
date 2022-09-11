9e897c2a81fabb6107eb1ea51fab456bfbb25913
wendy munyasi <wendymunyasi@gmail.com>
2022-09-11 22:11:30 +0300

    Make script executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that lists all states from the database hbtn_0e_0_usa"""
+"""script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 from sys import argv
 
