134a60098e3f64e374ab8857c0b7aa50f8994ec0
wendy munyasi <wendymunyasi@gmail.com>
2022-09-11 22:42:20 +0300

    Add script that lists states from db - task 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Script that lists all State objects that contain the letter a from the
+database hbtn_0e_6_usa"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(State.name.like('%a%'))\
+            .order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
