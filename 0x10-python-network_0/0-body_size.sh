8b6ba1311eefb017c1aa2f9d9894cb30c158331e
wendy munyasi <wendymunyasi@gmail.com>
2022-09-16 14:44:42 +0300

    Fix length of lines of script - task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL, and displays
-# the size of the body of the response.
-# The size must be displayed in bytes
-# You have to use curl
+# script that takes in a URL, sends a request to that URL, and displays size of the body of the response.
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
