a0e75912415dbc098ac002e2652c6812872b338d
wendy munyasi <wendymunyasi@gmail.com>
2022-09-16 15:23:37 +0300

    Attempt to fix codefails - task 4 - try 2

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -s -H "X-School-User-Id: 98" "$1"
+curl -s "$1" -H "X-School-User-Id: 98"
