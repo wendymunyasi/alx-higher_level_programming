13fc69f16339a6e413ec50980616f2e7fda39807
wendy munyasi <wendymunyasi@gmail.com>
2022-09-16 17:12:44 +0300

    Attempt to fix codefails - task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,44 @@
 #!/usr/bin/python3
 
-def find_peak(list_of_integers):
-    """function that finds a peak in a list of unsorted integers.
+def find_peak(list_of_integers, left=None, right=None):
+    """recursive function that finds a peak in a list of unsorted integers.
 
     Args:
         list_of_integers (list): a list of integers
 
+    Your algorithm must have the lowest complexity.
+    Note: there may be more than one peak in the list.
+
+    The most naive solution to this is to just go through each element
+    one-by-one and see if it's qualified as a peak. This solution will take
+    O(n) time complexity at the worst case and O(1) for space complexity
+    which is super for most algorithm problem. So, here comes the tricky
+    part, solve it with O(log(n)) time complexity!
+
+    Usually Binary Search is being used in sorted array(it could also mean
+    Bitonic array, or array that's sorted in some other ways), but this
+    one is a little bit different as we can't sort the array.
+
+    In Binary Search, we always check the middle value and see if it's
+    qualified to be a peak, if not then we change the start or end pointer
+    so that we get a new middle value.
+
     Returns:
-        int: largest number in list
+        int: peak(s)
     """
-    max_int = None
-    for number in list_of_integers:
-        if max_int is None or max_int < number:
-            max_int = number
-    return max_int
+    list_ = list_of_integers
+    # if there is no list of integers return None
+    if list_ == []:
+        return None
+    length = len(list_)
+
+    start, end = 0, length - 1
+    while start < end:
+        mid = start + (end - start) // 2
+        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
+            return list_[mid]
+        if list_[mid - 1] > list_[mid + 1]:
+            end = mid
+        else:
+            start = mid + 1
+    return list_[start]
