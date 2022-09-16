d52c8727f27d5027d76db293645e11a26f4d3089
wendy munyasi <wendymunyasi@gmail.com>
2022-09-16 15:06:11 +0300

    Add d in the cut command

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL and displays all HTTP methods the server will accept
-curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -" "
+curl -s -I -X OPTIONS "$1" | grep "Allow:" | cut -f2- -d" "
