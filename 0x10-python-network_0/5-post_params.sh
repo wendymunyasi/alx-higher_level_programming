eebd3043ac055842c7650c831803fbd9854c31b1
wendy munyasi <wendymunyasi@gmail.com>
2022-09-16 15:47:57 +0300

    Add script that sends POST request - task 5

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
