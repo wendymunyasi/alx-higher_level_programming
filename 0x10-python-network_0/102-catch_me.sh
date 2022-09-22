c592198f7f86563c08f831d3bd6c8a1f9304b385
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 00:05:49 +0300

    Attempt to fix codefails - task 9 - try 2

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X PUT -H "Origin:School" 0.0.0.0:5000/catch_me_3
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:School"
