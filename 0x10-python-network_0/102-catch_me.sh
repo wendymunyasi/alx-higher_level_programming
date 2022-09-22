7e61a17208b6c5ab0cd3145487eb293b4e0aa374
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 00:01:40 +0300

    Add script that sends a request - task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
+curl -sL -X PUT -H "Origin:School" 0.0.0.0:5000/catch_me_3
