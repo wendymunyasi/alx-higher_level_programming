5918fc0b89733eb6962127b0a25813ac69d9c65e
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 16:05:23 +0300

    Attempt to fix codefails - task 10 - try 6

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -18,8 +18,11 @@ if __name__ == "__main__":
             .format(username, repo_name)
         response = requests.get(commmits_url)
         json_obj = response.json()
-        for i in range(0, 10):
-            print("{}: {}".format(json_obj[i].get('sha'), json_obj[i]
-                                  .get('commit').get('author').get('name')))
+        for i, obj in enumerate(json_obj):
+            if i == 10:
+                break
+            if type(obj) is dict:
+                name = obj.get('commit').get('author').get('name')
+                print("{}: {}".format(obj.get('sha'), name))
     except ValueError as invalid_json:
         pass
