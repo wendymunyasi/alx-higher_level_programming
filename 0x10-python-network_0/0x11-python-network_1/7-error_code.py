974d50910dc18d67cf698f5a50001d110282e6ad
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 13:24:45 +0300

    Add script that sends rqst 2 specific URL - task 7

---
+++ 0x10-python-network_0/0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+If the HTTP status code is greater than or equal to 400, print: 
+Error code: followed by the value of the HTTP status code.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    body = response.text
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(body)
+        
