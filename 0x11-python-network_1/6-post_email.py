d218e385ddf2727eb324cb466964c0d19f849779
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 15:05:52 +0300

    Attempt to fix codefails - task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     payload = {
-        "email": "email"
+        "email": email
     }
     response = requests.post(url, data=payload)
     print(response.text)
