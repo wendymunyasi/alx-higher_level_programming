1023fdd60b26eab9e5e5517dd4b522f426344514
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 12:02:13 +0300

    Add script that fetches data - task 0

---
+++ 0x10-python-network_0/0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"Module that fetches https://alx-intranet.hbtn.io/status"
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
