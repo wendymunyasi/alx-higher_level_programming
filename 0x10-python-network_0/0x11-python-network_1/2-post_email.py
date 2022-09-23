002dcc7df78f0e41a21817fa32299afd5357be1f
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 12:29:03 +0300

    Add script that sends a POST request - task 2

---
+++ 0x10-python-network_0/0x11-python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter, and displays the body of the response
+(decoded in utf-8).
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    param = {
+        "email": "email"
+    }
+    query_string = urllib.parse.urlencode(param)
+    data = query_string.encode("ascii")
+    with urllib.request.urlopen(url, data) as response:
+        response_text = response.read().decode("utf-8")
+        print(response_text)
