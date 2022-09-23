4fb223bb3b2cbe182f7f6629047b7614c8d742c3
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 14:58:03 +0300

    Attempt to fix codefails - task 2 - try 3

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     param = {
-        "email": "email"
+        "email": email
     }
     query_string = urllib.parse.urlencode(param)
     data = query_string.encode("ascii")
