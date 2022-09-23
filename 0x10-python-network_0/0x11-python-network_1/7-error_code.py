4d0721ab720c6ed6aa5c3a022a08f2f5b795224c
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 13:25:14 +0300

    Fix pycodestyle errors

--- 0x10-python-network_0/0x11-python-network_1/7-error_code.py
+++ 0x10-python-network_0/0x11-python-network_1/7-error_code.py
@@ -2,7 +2,7 @@
 """
 script that takes in a URL, sends a request to the URL and displays the
 body of the response (decoded in utf-8).
-If the HTTP status code is greater than or equal to 400, print: 
+If the HTTP status code is greater than or equal to 400, print:
 Error code: followed by the value of the HTTP status code.
 """
 import sys
@@ -17,4 +17,3 @@ if __name__ == "__main__":
         print("Error code: {}".format(response.status_code))
     else:
         print(body)
-        
