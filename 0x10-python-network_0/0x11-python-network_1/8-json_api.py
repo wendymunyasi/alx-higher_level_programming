d77f003c7f468181b4133b48762132a8f495e95a
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 13:40:21 +0300

    Add script that sends a POST request - task 8

---
+++ 0x10-python-network_0/0x11-python-network_1/8-json_api.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+The letter must be sent in the variable q.
+If no argument is given, set q="".
+If the response body is properly JSON formatted and not empty, display the
+id and name like this: [<id>] <name>, Otherwise: Display Not a valid JSON
+if the JSON is invalid and Display No result if the JSON is empty
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    payload = {"q": "q"}
+    response = requests.post(url, data=payload)
+    try:
+        json_outp = response.json
+        if not json_outp:
+            print("No result")
+        else:
+            print("[{}] {}".format(json_outp.get("id"), json_outp.get("name")))
+    except Exception:
+        print("Not a valid JSON")
