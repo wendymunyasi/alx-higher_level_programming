2815f1739b83cf5b540f00f1fc8f0590248f0c72
wendy munyasi <wendymunyasi@gmail.com>
2022-09-23 15:49:15 +0300

    Add script that prints commits by author - task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+script that takes 2 arguments in order to list 10 commits (from the most
+recent to oldest) of the repository "rails" by the user "rails".
+Print all commits by: `<sha>: <author name>` (one by line)
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    try:
+        repo_name = sys.argv[1]
+        username = sys.argv[2]
+        commmits_url = "https://api.github.com/repos/{}/{}/commits" \
+            .format(username, repo_name)
+        response = requests.get(commmits_url)
+        json_obj = response.json()
+        for i in range(10):
+            sha = json_obj[i].get("sha")
+            commit = json_obj[i].get("commit").get("author").get("name")
+            print("{}: {}".format(sha, commit))
+    except ValueError as invalid_json:
+        pass
