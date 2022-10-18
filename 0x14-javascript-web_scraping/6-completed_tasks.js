6c7210c479527ecdacf95e2e9fe5493516a11328
wendy munyasi <wendymunyasi@gmail.com>
2022-10-18 23:22:40 +0300

    Add script that computes no. of tasks - task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+
+// The first argument is the API URL
+const baseURL = process.argv[2];
+request(baseURL, (error, body) => {
+  const aggregate = {};
+  if (error) {
+    console.log(error);
+  }
+  const json = JSON.parse(body);
+  json.forEach(element => {
+    if (element.completed) {
+      if (!aggregate[element.userId]) {
+        aggregate[element.userId] = 0;
+      }
+      aggregate[element.userId]++;
+    }
+  });
+  console.log(aggregate);
+});
