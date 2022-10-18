1f637425bff18f3c25edd0ed843fd2ef3bf516e2
wendy munyasi <wendymunyasi@gmail.com>
2022-10-18 23:09:17 +0300

    Add script that prints no. of movies - task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+
+//  first argument is the API URL
+const URL = process.argv[2];
+
+request(URL, (error, body) => {
+  if (error) {
+    console.log(error);
+  } else if (body) {
+    // Wedge Antilles is character ID 18 - use this ID for filtering the
+    // result of the API
+    const json = JSON.parse(body);
+    const charFilms = json.results.filter(
+      x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+    );
+    console.log(charFilms.length);
+  }
+});
