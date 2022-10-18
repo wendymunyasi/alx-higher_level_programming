460c703857090b64b3067c83e3bdd56cd8fdc175
wendy munyasi <wendymunyasi@gmail.com>
2022-10-18 22:28:06 +0300

    Add script that reads/ prins file content - task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const process = require('process');
+const fs = require('fs');
+
+// The first argument is the file path
+const file = process.argv[2];
+
+fs.readFile(file, 'utf8', function (err, data) {
+  // If an error occurred during the reading, print the error object
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
