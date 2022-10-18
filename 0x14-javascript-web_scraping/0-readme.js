126732ef17fd76e652f104165f348fd7c70aa0ab
wendy munyasi <wendymunyasi@gmail.com>
2022-10-18 22:32:17 +0300

    Add comments

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 // The first argument is the file path
 const file = process.argv[2];
-
+// The content of the file must be written in utf-8
 fs.readFile(file, 'utf8', function (err, data) {
   // If an error occurred during the reading, print the error object
   if (err) {
