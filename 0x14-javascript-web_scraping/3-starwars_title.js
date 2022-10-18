1c89f06212c23b276fd72fa3c156334b6e8f40ed
wendy munyasi <wendymunyasi@gmail.com>
2022-10-18 23:01:17 +0300

    Add script that prints title - task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const request = require('request');
+const baseUrl = 'https://swapi-api.hbtn.io/api/films';
+
+//  first argument is the movie ID
+const movieId = process.argv[2];
+
+request(`${baseUrl}/${movieId}/`, (error, body) => {
+  if (error) {
+    console.log(error);
+  }
+  const json = JSON.parse(body);
+  console.log(json.title);
+});
