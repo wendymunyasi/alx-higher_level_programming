6c95821386d859d71c9c242b3d447eee043e4295
wendy munyasi <wendymunyasi@gmail.com>
2022-10-26 15:40:49 +0300

    remove a few extra code

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -2,7 +2,5 @@
 // https://swapi-api.hbtn.io/api/people/5/?format=json
 const $ = window.$;
 $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
-  console.log(data);
   $('#character').append(data.name);
 });
-console.log('test');
